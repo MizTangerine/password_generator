90f5c254cd3fa7c9aecec2ca12762191386017af
Clarissa Reeve <clarissa.reeve@gmail.com>
2020-08-05 20:49:06 -0600

    cleaned up for consistency and readablity

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,29 +4,28 @@ const uppercaseEl = document.getElementById('uppercase');
 const lowercaseEl = document.getElementById('lowercase');
 const numbersEl = document.getElementById('numbers');
 const symbolsEl = document.getElementById('symbols');
-console.log(lengthEl, uppercaseEl, lowercaseEl, numbersEl, symbolsEl);
 var passwordText = document.querySelector("#password");
-const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-const lower = 'abcdefghijklmnopqrstuvwxyz'
-const number = '0123456789'
-const symbol = '!@#$%^&*()'
+
+// avalible characters
+const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lower = 'abcdefghijklmnopqrstuvwxyz';
+const number = '0123456789';
+const symbol = '!@#$%^&*~",-_+=/.';
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// console.log(generateBtn);
 
 function generatePassword() {
-  //code to be ran
-  console.log('generatePassword is running');
   //generate upper, lower, numbers, symbols
-  let length = lengthEl.value
+  let length = lengthEl.value;
   if (length < 8 || length > 128) {
     passwordText.value = 'Invalid Length! Please choose a number between 8 and 128.'
+    return 'Invalid Length'
   } else {
     if (uppercaseEl.checked == false && lowercaseEl.checked == false && numbersEl.checked == false && symbolsEl.checked == false) {
       passwordText.value = 'At lease one criteria must be selected.'
+      return 'Invalid Criteria Selection'
     } else {
-      console.log(uppercaseEl.checked, lowercaseEl.checked, numbersEl.checked, symbolsEl.checked)
       let acceptedLetters = [];
       let password = ''
       if (uppercaseEl.checked) {
@@ -41,24 +40,21 @@ function generatePassword() {
       if (symbolsEl.checked) {
         acceptedLetters = acceptedLetters + symbol
       }
-      console.log(acceptedLetters)
+      //for loop
       for (i = 0; i < length; i++) {
-        var index = Math.floor(Math.random() * acceptedLetters.length)
+        let index = Math.floor(Math.random() * acceptedLetters.length)
         password += acceptedLetters[index]
+
       }
       passwordText.value = password;
       return password
     }
-
   }
 }
-
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  console.log(password);
-
 }
 
 // Add event listener to generate button
